999b33cfbac71d6a918f333bf3647915a8821872
PsyduckLiu <25915043@qq.com>
2022-11-21 19:11:42 +0800

    tc-exp2

--- timedCommitment_experiment/src/open.go
+++ timedCommitment_experiment/src/open.go
@@ -8,13 +8,13 @@ import (
 
 func ForcedOped(c *big.Int, h *big.Int, rKSubOne *big.Int, rK *big.Int, k int) {
 	N := config.GetN()
-	fmt.Println("[Open]Get N is", N)
+	// fmt.Println("[Open]Get N is", N)
 
 	var kBig = big.NewInt(int64(k - 1))
 	times := new(big.Int)
 	times.Exp(bigTwo, kBig, nil)
 	times.Sub(times, bigOne)
-	fmt.Println("[Open]tims is", times)
+	fmt.Println("[Open]times is", times)
 
 	var index = big.NewInt(0)
 	r := new(big.Int).Set(rKSubOne)
@@ -23,14 +23,14 @@ func ForcedOped(c *big.Int, h *big.Int, rKSubOne *big.Int, rK *big.Int, k int) {
 		index.Add(index, bigOne)
 	}
 
-	fmt.Println("[Open]After forced open, r is", r)
+	// fmt.Println("[Open]After forced open, r is", r)
 
 	msg := new(big.Int)
 	msg.Xor(c, r)
-	fmt.Println("[Open]After forced open, msg is", msg)
+	// fmt.Println("[Open]After forced open, msg is", msg)
 
 	rSquare := new(big.Int).Set(r)
 	rSquare.Exp(rSquare, bigTwo, N)
-	fmt.Println("Verify", rSquare.Cmp(rK))
-
+	fmt.Println("[Open]Verify", rSquare.Cmp(rK))
 }
+
